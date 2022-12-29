8f0d9f0c1b1d944c24ec7a9fd0627ae3eb1bcfad
Krystian <kkonieczko@icloud.com>
2022-12-29 19:49:44 +0100

    Initial Commit

---
+++ EatIdeasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EatIdeasApp
+//
+//  Created by Krystian Konieczko on 29/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
