3738fdabff77c9dbd565d97476b075c101fc45b0
Krystian <kkonieczko@icloud.com>
2023-02-12 17:27:33 +0100

    add popup window, fix fetching dishes from ingredients

--- EatIdeasApp/Controller/ViewController.swift
+++ EatIdeasApp/Controller/ViewController.swift
@@ -12,18 +12,28 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var blurEffect: UIVisualEffectView!
+    @IBOutlet var popUpView: UIView!
+    @IBOutlet weak var ingridientsTableView: UITableView!
+    @IBOutlet weak var addTextField: UITextField!
     
     var randomManager = RandomManager()
     
-    @IBOutlet weak var ingridientsTableView: UITableView!
-    
-    @IBOutlet weak var blurEffect: UIView!
-    
+    var ingridients: [String] = [String]()
     
     var dishes : [RandomModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        blurEffect.bounds = self.view.bounds
+        popUpView.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width *  0.9, height: self.view.bounds.height * 0.4)
+        
+        ingridientsTableView.dataSource = self
+        ingridientsTableView.delegate = self
+        
+        addTextField.delegate = self
         
         
         tableView.dataSource = self
@@ -39,6 +49,7 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
+
     
     @IBAction func reloadButtonPressed(_ sender: UIButton) {
         randomManager.fetchDishes()
@@ -55,32 +66,67 @@ class ViewController: UIViewController {
             detailsViewController.recipeId = dishes[row].id
             detailsViewController.segueIdentifier = segue.identifier
         }
+    }
+    
+    //TODO: all works fine, next step is to add an option to allow users to delete ingridients if they want to
+    
+    @IBAction func searchByIngridientsButtonPressed(_ sender: UIButton) {
+        if ingridients.count != 0 {
+            randomManager.fetchDishesFromIngridients(ingridients: ingridients)
+            ingridients.removeAll()
+            ingridientsTableView.reloadData()
+        }
         
     }
+    
 }
 //MARK: tableView DataSource extension
 extension ViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dishes.count
+        
+        if tableView == self.tableView {
+            return dishes.count
+        } else {
+            return ingridients.count
+        }
+
     }
     // stworz cell i zwroc ja
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Dish", for: indexPath)
-        cell.textLabel?.text = dishes[indexPath.row].name
+        if tableView == self.tableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Dish", for: indexPath)
+            cell.textLabel?.text = dishes[indexPath.row].name
+            
+            //zawijanie wierszy w komorce
+            cell.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+            cell.textLabel?.numberOfLines = 0
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Ingridient", for: indexPath)
+            cell.textLabel?.text = ingridients[indexPath.row]
+            
+            //zawijanie wierszy w komorce
+            cell.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+            cell.textLabel?.numberOfLines = 0
+            return cell
+
+        }
         
-        //zawijanie wierszy w komorce
-        cell.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
-        cell.textLabel?.numberOfLines = 0
         
-        return cell
+        
     }
     
 }
 //MARK: Tableview delegate extension
 extension ViewController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedCell = tableView.cellForRow(at: indexPath)
-        performSegue(withIdentifier: "goToDetails", sender: selectedCell)
+        if tableView == tableView {
+            let selectedCell = tableView.cellForRow(at: indexPath)
+            performSegue(withIdentifier: "goToDetails", sender: selectedCell)
+        } else {
+            
+        }
+       
     }
     
 }
@@ -105,9 +151,85 @@ extension ViewController : RandomManagerDelegate {
     func didRecieveDishFromIngridients(_ randomManager: RandomManager, returned: [DishFromIngridientsModel]) {
         //managed to catch all the items in here, what to do with that
         
+        for item in returned {
+            print(item.title)
+        }
+        
             
         
     }
     
     
 }
+//MARK: - PopUpWindow Animations
+extension ViewController {
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        
+        ingridients.removeAll()
+        ingridientsTableView.reloadData()
+        animateIn(desiredView: blurEffect)
+        animateIn(desiredView: popUpView)
+    }
+    
+    @IBAction func exitButtonPressed(_ sender: UIButton) {
+        animateOut(desiredView: popUpView)
+        animateOut(desiredView: blurEffect)
+        
+    }
+    
+    func animateIn(desiredView: UIView) {
+        let backgroundView = self.view
+        
+        backgroundView!.addSubview(desiredView)
+        
+        desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        desiredView.alpha = 0
+        desiredView.center = backgroundView!.center
+        
+        UIView.animate(withDuration: 0.3) {
+            desiredView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            desiredView.alpha = 1
+            
+        }
+    }
+    func animateOut(desiredView: UIView){
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            desiredView.alpha = 0
+        }, completion:  { _ in
+            desiredView.removeFromSuperview()}
+        )
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if addTextField.text != "" {
+            ingridients.append(addTextField.text!)
+        }
+            
+        ingridientsTableView.reloadData()
+        
+        addTextField.endEditing(true)
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        addTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if addTextField.text != "" {
+            return true
+        } else {
+            addTextField.placeholder = "Insert an ingridient here!"
+            return false
+        }
+    }
+}
+
+
